2038b4834d1984f11cba291fdd131f13a39d7caa
harimohan <harimohan.info@gmail.com>
2023-10-17 15:02:09 +0530

    class based or function based

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import HelloReactClassbased from './components/Hello_Word/HelloReactClassbased';
+
+import HelloReactfunctionalBased from './components/Hello_Word/HelloReactfunctionalBased'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <HelloReactClassbased/>
+     <HelloReactfunctionalBased/>
     </div>
   );
 }
---
+++ src/components/Hello_Word/HelloReactClassbased.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class HelloReactClassbased extends Component {
+    constructor(props){
+        super(props);
+    };
+  render() {
+    return (
+      <div>
+      <h1>Hello from React Class based component</h1>
+      </div>
+    )
+  }
+}
